b10c9a57dd97b08269a0e6dc60e7c3ffe560dc66
Matthew Lucas <lucasmatthew@hotmail.com>
2023-07-07 17:10:12 +0100

    Newsletter

---
+++ elixir/newsletter/lib/newsletter.ex
@@ -0,0 +1,30 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    File.read!(path)
+      |> String.split("\n")
+      |> Enum.filter(& &1 != "")
+  end
+
+  def open_log(path) do
+    File.open!(path, [:write])
+  end
+
+  def log_sent_email(pid, email) do
+    IO.write(pid, email <> "\n")
+  end
+
+  def close_log(pid) do
+    File.close(pid)
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    emails = read_emails(emails_path)
+    log = open_log(log_path)
+    for email <- emails do
+      with :ok <- send_fun.(email) do
+        log_sent_email(log, email)
+      end
+    end
+    close_log(log)
+  end
+end
